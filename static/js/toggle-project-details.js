6cd3e7d01d13f63cf508d482bfc677b906e1323e
Naeem Khan <naeemukhan14@gmail.com>
2021-06-15 22:54:43 +0400

    Completed base template and static files linking for main page.

---
+++ static/js/toggle-project-details.js
@@ -0,0 +1,7 @@
+function openNav(div_id) {
+    document.getElementById(div_id).style.height = "100%";
+}
+
+function closeNav(clicked_id) {
+    document.getElementById(clicked_id).style.height = "0%";
+}
